4b1f1910ddbb214c221b00756b4f509bfb86c00e
alondon17 <alonaloneast@gmail.com>
2021-01-12 12:42:59 +0200

    Delete src directory

--- src/utility/utility.js
+++
@@ -1,9 +0,0 @@
-export const pather=(obj,path)=>{
-    if(typeof path==='string')
-    return obj[path]
-    var result=obj
-    path.forEach(element => {
-        result=result[element]
-    });
-    return result
-}
